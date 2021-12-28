8ec2330b7545b41f71765638cdc67ee1c8d210b1
Chris Ha <chunghha@users.noreply.github.com>
2021-12-27 22:36:38 -0600

    chore(theme): rosepine color theme

--- packages/theme/lib/src/template.theme.dart
+++ packages/theme/lib/src/template.theme.dart
@@ -1,5 +1,6 @@
 // default constants
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const double defaultElevation = 4;
 const double defaultOffset = 16;
@@ -8,4 +9,24 @@ const double defaultRadius = 16;
 
 final defaultBoxShadowColor = Color(0xFFeeeeee).withOpacity(0.5);
 
-// define theme related
+// rosepine color palette
+// https://rosepinetheme.com/palette.html#rose-pine
+final baseColor = Color(0xFF191724);
+final surfaceColor = Color(0xFF1f1d2e);
+final overlayColor = Color(0xFF26233a);
+final inactiveColor = Color(0xFF555169);
+final subtleColor = Color(0xFF6e6a86);
+final textColor = Color(0xFFe0def4);
+final loveColor = Color(0xFFeb6f92);
+final goldColor = Color(0xFFf6c177);
+final roseColor = Color(0xFFebbcba);
+final pineColor = Color(0xFF31748f);
+final foamColor = Color(0xFF9ccfd8);
+final irisColor = Color(0xFFc4a7e7);
+final highlightColor = Color(0xFF2a2837);
+final highlightInactiveColor = Color(0xFF211f2d);
+final highlightOverlayColor = Color(0xFF3a384a);
+
+// text styles
+final TextStyle defaultTextStyle =
+    GoogleFonts.roboto(color: textColor, fontSize: 14);
