3ffc7f342e8e5f1de2414df4dba61267ad017b01
Chris Ha <chunghha@users.noreply.github.com>
2024-02-15 17:35:35 -0600

    build(lib): flutter v3.19.x

--- packages/model/lib/src/placeholder/user/user_geo.model.freezed.dart
+++ packages/model/lib/src/placeholder/user/user_geo.model.freezed.dart
@@ -12,7 +12,7 @@ part of 'user_geo.model.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 UserGeoModel _$UserGeoModelFromJson(Map<String, dynamic> json) {
   return _UserGeoModel.fromJson(json);
